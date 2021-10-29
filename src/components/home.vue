1a27ef53b4e0cf3d75e03c6324c14ae98bf964a1
cksgnlekt12 <cksgnlekt1@naver.com>
2021-10-29 11:56:15 +0900

    20211029 vue(2)

--- src/components/home.vue
+++ src/components/home.vue
@@ -19,14 +19,27 @@
   <!-- 반복문 -->
   <div v-for="(i,no) in is" :key="i*2">for문 테스트 {{i}}/{{is}} ({{no}})</div>
   <!-- 이벤트 바인딩 -->
-  <div v-on:click="testLog('로그띄워라..1')">이벤트 바인딩 (클릭하면 로그)</div>
-  <div @click="testLog('로그띄워라..2')">이벤트 바인딩@방식 (클릭하면 로그)</div>
+  <div class="eventBinding">{{testLog()}}
+    <div v-on:click="testLog()">이벤트 바인딩 (클릭하면 로그)</div>
+    <div @click="testLog()">이벤트 바인딩@방식 (클릭하면 로그)</div>
+  </div>
 
   <!-- 양방향 바인딩 -->
   <div>
     <!-- input 태그로 v-model 에서 데이터 바인딩의 보간없이 작성하여 미리 입력된 예제. -->
     <input v-model="message" placeholder="입력" />
     <p>메시지: {{ message }}</p>
+    <input type="button" value="초기화" @click="message='초기화'">
+  </div>
+
+  <!-- methods, computed, watch 테스트 -->
+  <div class="sub">
+      methods - count: {{methodsCount()}}<br>
+      <input type="button" value="증가" @click="countMethods++">
+      <input type="button" value="감소" @click="countMethods--"><br>
+      computed - count: {{computedCount}}<br>
+      <input type="button" value="증가" @click="countComputed++">
+      <input type="button" value="감소" @click="countComputed--"><br>
   </div>
 </template>
 
@@ -42,20 +55,38 @@ export default {
       ifTest: false,
       ifTest2: true,
       is: 5,
-      no: 1
+      no: 1,
+      countMethods: 0,
+      countComputed: 0
     }
   },
   methods: {
-    testLog : function (msg){
-      console.log(msg);
+    testLog (){
+      console.log('methods 테스트');
+      return this.message;
+    },
+    methodsCount(){
+      console.log('methods!');
+      // return this.countMethods;
     }
   },
   computed: {
-    computeLog : function (msg){
-      console.log('Computed 테스트:',msg);
-      return msg;
+    computedCount() {
+      console.log('computed');
+      return this.countComputed;
     }
   },
+  watch: {
+    // 이쪽은 data의 이름과 일치해야 함(?)
+    // message(){
+    //   console.log('watch Test')
+    // }
+  },
+  /* methods, compute, watch 차이점.
+  methods : template 내의 어떤 것이든 변경되면 작동
+  computed : template 내의 값 중 함수와 연결된 값이 바뀔때 작동
+  watch : template 내의 값이 지정된 변수가 값만 변경됐다 하면 작동
+  */
   name: 'home',
   props: {
     msg: String
